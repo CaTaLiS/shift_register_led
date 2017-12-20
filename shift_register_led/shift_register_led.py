e4188064c1a00f56a768d937bee583503ae33874
CaTaLiS <sylwiusz.kosacz@gmail.com>
2017-12-20 18:44:31 +0100

    added parsing args, interactive mode

--- shift_register_led/shift_register_led.py
+++ shift_register_led/shift_register_led.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import argparse
+import sys
 
 DATA_PIN = 4 #DS
 CLOCK_PIN = 17 #SH_CP
 LATCH_PIN = 27 #ST_CP
 
 def setup():
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(DATA_PIN, GPIO.OUT)
     GPIO.setup(CLOCK_PIN, GPIO.OUT)
@@ -25,16 +28,34 @@ def shiftOut(byte):
         GPIO.output(CLOCK_PIN, GPIO.LOW)
     GPIO.output(LATCH_PIN, GPIO.HIGH)
     
-def count_on_leds_binary():
-    print('Counting from 0 to 255 on LEDs.')
-    for number in range(0, 255):
-        print(number)
-        shiftOut(number)
-        time.sleep(1)
+def count_on_leds_binary(endless):
+    print('Counting from 0 to 255 on LEDs. Endless: {}'.format(endless))
+    while (True):
+        for number in range(0, 256):
+            print(number)
+            shiftOut(number)
+            time.sleep(0.1)
+        if not endless:
+            break
+    
+def show_binary_number():
+    while (True):
+        print('Enter DEC 0-255: ')
+        input_number = int(sys.stdin.readline())
+        shiftOut(input_number)
+        print('{} send to led through shift register'.format(input_number))
     
 if __name__ == '__main__':
     setup()
     try:
-        count_on_leds_binary()
+        parser = argparse.ArgumentParser(description='Plays with shift register (74HC595).')
+        parser.add_argument('-c', '--count', action='store_true', default=False, dest="count", help="counting from 0 to 255 with shift register")
+        parser.add_argument('-e', '--endless', action='store_true', default=False, dest="endless", help="endless counting")
+        parser.add_argument('-i', '--interactive', action='store_true', default=True, dest="input", help="interactive input mode")
+        args = parser.parse_args()
+        if args.count:
+            count_on_leds_binary(args.endless)
+        if args.input:
+            show_binary_number()
     except KeyboardInterrupt:
         destroy()
